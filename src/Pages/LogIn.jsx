b9ec7fc916263a9c905c880b182446d4cac47660
Muhammad <shalauddinahmedshipon@gmail.com>
2024-04-27 12:09:01 -0800

    login and register route ui design done

--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -1,13 +1,14 @@
+import { Link } from "react-router-dom";
 
 
 const LogIn = () => {
   return (
     <div className="flex justify-center items-center">
 
-      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-10 shadow-xl bottom-2 border-gray-400 mt-20">
+      <div className="flex flex-col max-w-md p-6 rounded-3xl sm:p-10 shadow-xl border-2 border-gray-400 mt-20">
         <div className="mb-8 text-center">
           <h1 className="my-3 text-4xl font-bold">Log in</h1>
-          <p className="text-sm">Sign in to access your account</p>
+          <p className="text-sm">Log in to access your account</p>
         </div>
         <form noValidate="" action="" className="space-y-12">
           <div className="space-y-4">
@@ -27,8 +28,44 @@ const LogIn = () => {
             <div>
               <button type="button" className="w-full px-8 py-3 font-semibold rounded-md bg-indigo-500 text-white">Log in</button>
             </div>
+    
+            <div className="flex items-center pt-4 space-x-1">
+		<div className="flex-1 h-px sm:w-16 bg-white"></div>
+		<p className="px-3 text-sm ">Login with social accounts</p>
+		<div className="flex-1 h-px sm:w-16 bg-white"></div>
+	</div>
+	<div className="flex justify-center space-x-4">
+		<button aria-label="Log in with Google" className="p-3 hover:scale-110 rounded-sm">
+    <svg viewBox="0 0 48 48" className="w-10 h-5 fill-current">
+  
+  <clipPath id="g">
+    <path d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/>
+  </clipPath>
+  <g className="colors" clipPath="url(#g)">
+    <path fill="#FBBC05" d="M0 37V11l17 13z"/>
+    <path fill="#EA4335" d="M0 11l17 13 7-6.1L48 14V0H0z"/>
+    <path fill="#34A853" d="M0 37l30-23 7.9 1L48 0v48H0z"/>
+    <path fill="#4285F4" d="M48 48L17 24l-4-3 35-10z"/>
+  </g>
+</svg>
+		</button>
+		
+		<button  aria-label="Log in with GitHub" className="p-3 hover:scale-110  rounded-sm">
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" className="w-10 h-5 fill-current">
+				<path d="M16 0.396c-8.839 0-16 7.167-16 16 0 7.073 4.584 13.068 10.937 15.183 0.803 0.151 1.093-0.344 1.093-0.772 0-0.38-0.009-1.385-0.015-2.719-4.453 0.964-5.391-2.151-5.391-2.151-0.729-1.844-1.781-2.339-1.781-2.339-1.448-0.989 0.115-0.968 0.115-0.968 1.604 0.109 2.448 1.645 2.448 1.645 1.427 2.448 3.744 1.74 4.661 1.328 0.14-1.031 0.557-1.74 1.011-2.135-3.552-0.401-7.287-1.776-7.287-7.907 0-1.751 0.62-3.177 1.645-4.297-0.177-0.401-0.719-2.031 0.141-4.235 0 0 1.339-0.427 4.4 1.641 1.281-0.355 2.641-0.532 4-0.541 1.36 0.009 2.719 0.187 4 0.541 3.043-2.068 4.381-1.641 4.381-1.641 0.859 2.204 0.317 3.833 0.161 4.235 1.015 1.12 1.635 2.547 1.635 4.297 0 6.145-3.74 7.5-7.296 7.891 0.556 0.479 1.077 1.464 1.077 2.959 0 2.14-0.020 3.864-0.020 4.385 0 0.416 0.28 0.916 1.104 0.755 6.4-2.093 10.979-8.093 10.979-15.156 0-8.833-7.161-16-16-16z"></path>
+			</svg>
+		</button>
+	</div>
+
+
+
+
+
+
+
+
             <p className="px-6 text-sm text-center">Do not have an account yet?
-              <a rel="noopener noreferrer" href="#" className="hover:underline text-indigo-400">Register</a>.
+              <Link to={"/register"}><a rel="noopener noreferrer" href="#" className="hover:underline text-indigo-400">Register</a>.</Link>
             </p>
           </div>
         </form>
--- src/Pages/Registration.jsx
+++ src/Pages/Registration.jsx
@@ -1,9 +1,60 @@
-
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { IoMdEye } from "react-icons/io";
+import { IoMdEyeOff } from "react-icons/io";
 
 const Registration = () => {
+  const [showPassword,setShowPassword] = useState(false);
   return (
-    <div>
-      Please Registration
+    <div className="flex justify-center item-center">
+    
+
+      <div  className="w-full  max-w-md p-4 mt-10 rounded-3xl shadow-xl border-2 border-gray-400 sm:p-8 ">
+	<h2 className="mb-3 text-3xl font-semibold text-center ">Create your account</h2>
+  <form data-aos="fade-up" data-aos-duration="3000"  className="space-y-8 ">
+		<div className="space-y-4">
+			<div className="space-y-2">
+				<label htmlFor="name" className="block text-sm ">Name</label>
+				<input type="text" required name="name" id="name" placeholder="Your Full Name" className="w-full px-3 py-3 border rounded-md border-gray-700 bg-gray-200 text-gray-900 focus:border-violet-400" />
+			</div>
+			<div className="space-y-2">
+				<label htmlFor="email" className="block text-sm " >Email address</label>
+				<input type="email" name="email" id="email" placeholder="Your Email" className="w-full px-3 py-3 border rounded-md border-gray-700 bg-gray-200 text-gray-900 focus:border-violet-400" />
+			</div>
+			
+			<div className="space-y-2">
+				<label htmlFor="photo" className="block text-sm " >Photo</label>
+				<input type="text" name="photo" id="photo" placeholder="Photo Url" className="w-full px-3 py-3 border rounded-md border-gray-700 bg-gray-200 text-gray-900 focus:border-violet-400" />
+			</div>
+			<div className="space-y-2">
+				<div className="flex justify-between">
+					<label htmlFor="password" className="text-sm ">Password</label>
+					<a rel="noopener noreferrer" href="#" className="text-xs hover:underline text-gray-400">Forgot password?</a>
+				</div>
+				<div className="flex items-center w-full relative">
+				<input type={showPassword? "text":"password"} required  name="password" id="password" placeholder="Enter Your Password" className="w-full px-3 py-3 border rounded-md border-gray-700 bg-gray-200 text-gray-900 focus:border-violet-400" />
+				<span className="right-3 text-gray-500 absolute  text-2xl" onClick={()=>setShowPassword(!showPassword)}>
+        {showPassword?<IoMdEyeOff />
+         : <IoMdEye />
+        
+        }
+        </span>
+				</div>
+			</div>
+		</div>
+		<button className="btn bg-green-400 font-bold text-white rounded-lg  hover:scale-105 w-full">Register</button>
+	</form>
+	<p className="text-sm text-center mt-3 ">Already have an account?
+	<Link to={"/log-in"}><p className="focus:underline hover:underline text-green-400">log in here</p></Link>
+	</p>
+
+  
+
+
+	
+
+	
+</div>
     </div>
   );
 };
