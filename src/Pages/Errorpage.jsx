d54ed0b17c26fbca49195a3f2c40eb7d3d75c821
Muhammad <shalauddinahmedshipon@gmail.com>
2024-04-30 04:23:55 -0800

    Error route design done

--- src/Pages/Errorpage.jsx
+++ src/Pages/Errorpage.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
 
 
 const Errorpage = () => {
   return (
-    <div>
-      404
-    </div>
+    <div className="flex items-center justify-center w-full min-h-screen bg-black">
+    
+   <div className="text-white  ">
+   <h1 className="text-7xl font-bold text-center">404</h1>
+   <p className="text-xl">Page Not Found</p>
+  <Link to={"/"}> <button className="bg-blue-600 p-4 my-5 rounded-xl text-white">Back to Home</button></Link>
+ </div>
+ </div>
   );
 };
 
